a5d22d3a6e9a3d48365850a699e6474802ca607b
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-07-30 12:54:50 +0530

    Create section in app.jsx

--- Bank modern app/src/App.jsx
+++ Bank modern app/src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import styles from './style';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>Hellow, World</h1>
+
+    <div className={`${styles.paddingX} ${styles.felxCenter }`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styels.boxwidth}`}>
+        Hero
+      </div>
+    </div>
+ 
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styels.boxwidth}`}>
+        Stats
+        Business 
+        Billing 
+        CardDeal 
+        Testingmonials 
+        Clients 
+        ClickToAction 
+        Footer
+      </div>
+    </div>
+
   </div>
   );
 
