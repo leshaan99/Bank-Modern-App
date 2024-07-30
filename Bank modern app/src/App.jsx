e0acd518f0bd459f39b160dbca1bc136e64693d0
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-07-30 14:16:27 +0530

    Import components to app.jsx

--- Bank modern app/src/App.jsx
+++ Bank modern app/src/App.jsx
@@ -1,5 +1,16 @@
 import styles from './style';
 
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Stats from './components/Stats';
+import Business from './components/Business';
+import Billing from './components/Billing';
+import CardDeal from './components/CardDeal';
+import Testingmonils from './components/Testingmonils';
+import Clients from './components/Clients';
+import CTA from './components/CTA';
+import Footer from './components/Footer'; 
+
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
 
@@ -23,7 +34,7 @@ const App = () => (
         CardDeal 
         Testingmonials 
         Clients 
-        ClickToAction 
+        CTA
         Footer
       </div>
     </div>
