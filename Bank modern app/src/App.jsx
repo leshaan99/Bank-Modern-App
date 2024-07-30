7ee91175a1eda8d61e71810823d595baba72fd8c
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-07-30 14:43:06 +0530

    Update app.jsx

--- Bank modern app/src/App.jsx
+++ Bank modern app/src/App.jsx
@@ -6,7 +6,7 @@ import Stats from './components/Stats';
 import Business from './components/Business';
 import Billing from './components/Billing';
 import CardDeal from './components/CardDeal';
-import Testingmonils from './components/Testingmonils';
+import Testimonials from './components/Testimonials';
 import Clients from './components/Clients';
 import CTA from './components/CTA';
 import Footer from './components/Footer'; 
@@ -16,26 +16,26 @@ const App = () => (
 
     <div className={`${styles.paddingX} ${styles.felxCenter }`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar/>
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styels.boxwidth}`}>
-        Hero
+        <Hero/>
       </div>
     </div>
  
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styels.boxwidth}`}>
-        Stats
-        Business 
-        Billing 
-        CardDeal 
-        Testingmonials 
-        Clients 
-        CTA
-        Footer
+        <Stats/>
+        <Business/>
+        <Billing/>
+        <CardDeal/>
+        <Testimonials/>
+        <Clients/>
+        <CTA/>
+        <Footer/>
       </div>
     </div>
 
