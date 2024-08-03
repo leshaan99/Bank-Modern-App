ca8722b199e1a466fca6097f252ceb1e0c11eadc
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-08-03 19:59:42 +0530

    Create getstarted.jsx

--- Bank modern app/src/components/GetStarted.jsx
+++ Bank modern app/src/components/GetStarted.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import styles from "../style"
+import { arrowUp } from "../assets"
+
 
 const GetStarted = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  const gradientStyle = {
+    background: 'linear-gradient(to right, #5ce1e8, #5ce1e8, #ffffff)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+  };
+
+  <div style={gradientStyle} className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full p-[2px]`}>
+
+  </div>
+
 }
 
 export default GetStarted
