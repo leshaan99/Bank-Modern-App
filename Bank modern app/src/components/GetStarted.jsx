22884789bb998a88ad27b9ed1403ebe8193feffa
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-08-05 15:19:50 +0530

    Update index.css

--- Bank modern app/src/components/GetStarted.jsx
+++ Bank modern app/src/components/GetStarted.jsx
@@ -1,23 +1,16 @@
 import styles from '../style';
 import { arrowUp } from '../assets';
 
-const GetStarted = () => {
-    const gradientStyle = {
-      background: 'linear-gradient(to right, #5ce1e8, #5ce1e8, #ffffff)',
-      WebkitBackgroundClip: 'text',
-      WebkitTextFillColor: 'transparent',
-    }
-  
-    return (
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-gradient-to-r from-[#5ce1e8] via-[#5ce1e8] to-[#ffffff] cursor-pointer p-[2px]`}>
+const GetStarted = () => (
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient cursor-pointer p-[2px]`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
-          <p className="font-poppins font-medium text-[22px] leading-[23px]">
+          <p className="font-poppins font-medium text-[22px] leading-[23px] mr-2">
             <span style={gradientStyle}>
               Get 
             </span>
           </p>
-            <img src={arrowUp} alt ="arrow" className="w-[23px] h-[23px] object-contain"/>
+          <img src={arrowUp} alt ="arrow" className="w-[23px] h-[23px] object-contain"/>
         </div>
         <p className="font-poppins font-medium text-[22px] leading-[23px]">
           <span style={gradientStyle}>
@@ -26,7 +19,6 @@ const GetStarted = () => {
         </p>
       </div>
     </div>
-    )
-}
+);
 
 export default GetStarted
