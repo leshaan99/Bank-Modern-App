be952bdc4600d47a667ffae17b9abf6d161f3725
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-08-05 16:36:00 +0530

    Finalized stats section

--- Bank modern app/src/components/GetStarted.jsx
+++ Bank modern app/src/components/GetStarted.jsx
@@ -6,16 +6,12 @@ const GetStarted = () => (
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
           <p className="font-poppins font-medium text-[22px] leading-[23px] mr-2">
-            <span style={gradientStyle}>
-              Get 
-            </span>
+            <span className="text-gradient">Get</span>
           </p>
           <img src={arrowUp} alt ="arrow" className="w-[23px] h-[23px] object-contain"/>
         </div>
         <p className="font-poppins font-medium text-[22px] leading-[23px]">
-          <span style={gradientStyle}>
-            Started
-          </span>
+          <span className="text-gradient">Started</span>
         </p>
       </div>
     </div>
